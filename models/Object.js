f54ca8a365ce686464001c7dc2bd3e5fc8f117ca
habert_j <habert_j@etna-alternance.net>
2022-02-09 20:26:33 +0100

    adding object model

---
+++ models/Object.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+var objectSchema = mongoose.Schema({
+    title : {
+        type : String
+    },
+    image : {
+        type : String
+    },
+    description : {
+        type : String
+    },
+    createdBy : {
+        type : String
+    },
+    modifiedBy : {
+        type : String
+    },
+    creationDate : {
+        type : Date,
+        default : Date.now()
+    },
+    lastModification : {
+        type : Date,
+        default : Date.now()
+    }
+
+})
+
+module.exports = mongoose.model("ObjectPost",objectSchema)
