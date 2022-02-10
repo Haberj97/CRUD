f2ab12ff219246e18fb4231f70a3912bbbed720f
habert_j <habert_j@etna-alternance.net>
2022-02-10 20:12:11 +0100

    add fist view with form for creation new object

---
+++ client/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>  
+        <Nav/>
+        <nuxt id ="main" />
+    </div>
+</template>
+<script>
+import Nav from "./nav.vue";
+export default{
+    components : {
+        Nav
+    }
+}
+</script>
+<style scoped>
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="card">
-    <h1>Hello World</h1>
+  <div class="container card objectPostDesign">
+    <h1>Create an Object post</h1>
+    <div class="mb-3">
+      <label for="title" class="form-label">Post Title</label>
+      <input type="text" class="form-control" id="title" placeholder="Please enter the Object name">
+    </div>
+    <div class="mb-3">
+      <label for="image" class="form-label">image</label>
+      <input class="form-control" type="file" id="image">
+    </div>
+    <div class="mb-3">
+      <label for="description" class="form-label">add a description</label>
+      <textarea class="form-control" id="description" rows="3"></textarea>
+    </div>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <button type="button" class="btn btn-outline-primary btn-lg">Submit</button>
+    </div>
   </div>
 </template>
-
 <script>
-
+  import Nav from "../layouts/nav.vue";
+  export default{
+      component : {
+          Nav
+      }
+  }
 </script>
-
 <style scoped>
-
+  .objectPostDesign {
+    margin-top: 20px;
+    padding : 20px;
+  }
 </style>
